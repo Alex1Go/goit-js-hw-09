4f5db6b65983ceff3a6d25373168178f58da733a
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-06-29 23:28:47 +0300

    update HW 9

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,16 @@
 import Notiflix from 'notiflix';
 const form = document.querySelector('.form');
+const firstDelay = document.querySelector('[name="delay"]');
+const delayStep = document.querySelector('[name="step"]');
+const amountInput = document.querySelector('[name="amount"]');
 form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(evt) {
   evt.preventDefault();
-  const { delay, step, amount } = evt.target.elements;
-  delay.value;
-  step.value;
-  amount.value;
+   //const { delay, step, amount } = evt.target.elements;
+  let delay = Number(firstDelay.value);
+  const step = Number(delayStep.value);
+  const amount = Number(amountInput.value);
 
   for (let i = 1; i <= amount; i += 1) {
     createPromise(i, delay)
@@ -17,8 +20,10 @@ function onFormSubmit(evt) {
      .catch(({ position, delay }) => {
       Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
     });
+    delay += step;
   }
 }; 
+
 function createPromise(position, delay) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
