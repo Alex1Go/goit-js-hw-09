cef2dcb157ef47a7eac36e0142079c4236bba218
Alex1Go <125027886+Alex1Go@users.noreply.github.com>
2023-06-30 07:54:23 +0000

    Deploying to gh-pages from @ Alex1Go/goit-js-hw-09@dbdc1a759be930a3ced5e21f3093f3863223338d 🚀

---
+++ 01-color-switcher.3443c7be.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;e.addEventListener("click",(()=>{d=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e&&(e.disabled=!0,t.disabled=!1)})),t.addEventListener("click",(()=>{clearInterval(d),t&&(e.disabled=!1,t.disabled=!0)}));
+//# sourceMappingURL=01-color-switcher.3443c7be.js.map
